ba254581b556079fb9e29e2af208d296ff155d0d
Manishajangid26 <manishajangid12665@gmail.com>
2024-12-30 18:17:24 +0530

    adding while loop

---
+++ program7.py
@@ -0,0 +1,9 @@
+# Write a program that take a number as input and print counting till that number.
+
+
+
+num = int(input("enter any number :"))
+i = 0
+while i<num:
+    print(i + 1)
+    i = i+1
---
+++ program8.py
@@ -0,0 +1,9 @@
+# Write a program that take a number as input and print it's table.
+
+
+num = int(input("enter any no :"))
+i = 1
+while i<=10:
+    print(num,"*",i,"=",num*i)
+    i = i + 1
+     
