3976d584dd11caca827e482a5bfff35812511a7c
Manishajangid26 <manishajangid12665@gmail.com>
2025-01-02 12:38:47 +0530

    add sum of 10 number

---
+++ printsum.py
@@ -0,0 +1,40 @@
+# 3. Write a program that take 10 number from user and print sum of all numbers.
+
+
+total = 0
+count = 0
+while count<10:
+    num = int(input("enter 10 number :"))
+    total+=num
+    count+=1
+    print("Total=",total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = int(input("enter no :"))
+# b = int(input("enter no :"))
+# c = int(input("enter no :"))
+# d = int(input("enter no :"))
+# e = int(input("enter no :"))
+# f = int(input("enter no :"))
+# g = int(input("enter no :"))
+# h = int(input("enter no :"))
+# i = int(input("enter no :"))
+# j = int(input("enter no :"))
+
+# Total = a+b+c+d+e+f+g+h+i+j
+
+# print(Total)
