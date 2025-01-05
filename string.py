b533be19ca42ccc8188b7a5e830b32326e8ea398
Manishajangid26 <manishajangid12665@gmail.com>
2025-01-05 21:54:38 +0530

    adding string

---
+++ string.py
@@ -0,0 +1,25 @@
+# string function----
+
+# str = "i am study python"
+# print(str.endswith("hon"))
+
+
+
+
+# str = "i am study python"
+# print(str.capitalize())
+
+
+
+
+
+# str = "i am study python"
+# print(str.find("s"))
+
+
+
+
+
+str = "i am from studing python from online"
+print(str.count("from"))
+
