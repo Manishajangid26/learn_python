64816551479ed7ed060dbc29b9d593c82b9daa54
Manishajangid26 <manishajangid12665@gmail.com>
2025-01-15 16:25:24 +0530

    add prime no and reversing no

---
+++ reverse.py
@@ -0,0 +1,27 @@
+# Write a function reverseNumber() that takes a positive integer as input and returns the reversed number. For example, if the input is 123, the function should return 321.
+ 
+
+def rev_str(data):
+    data = str(data)
+    return data[ ::-1 ]
+
+
+print(rev_str(12345))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
