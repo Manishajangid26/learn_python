f1e6b1d6188290e2174f5e37448b6f217b2f04a6
Manishajangid26 <manishajangid12665@gmail.com>
2025-01-15 16:26:44 +0530

    prime no

---
+++ primeno.py
@@ -0,0 +1,32 @@
+# write a program to take a int an input and print  if the number is prime or not.
+
+num = int(input("enter a number"))
+
+for x in range (2 , num):
+    if num % x == 0:
+        temp_var = False
+        break
+    else:
+        temp_var = True
+
+if temp_var:
+        print("prime")
+else:
+        print("not prime")           
+
+
+ 
+
+
+
+
+
+
+
+# num = int(input("enter a no :"))
+# if num>1:
+#     for i in range(2 , num):
+#         if(num % i) == 0:
+#             print(num , "no is prime")
+#         else:
+#             print(num , "no is not prime")    
