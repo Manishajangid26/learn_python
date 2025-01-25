b9919765df11d627292f73a562b034ec0c6bbfd7
Manishajangid26 <manishajangid12665@gmail.com>
2025-01-25 16:23:38 +0530

    class program

---
+++ class.py
@@ -0,0 +1,10 @@
+class Animal:
+    janver = ["cow","goat","cat","dog"]
+    pakshi = ["owl","peacock","dove","parrot"]
+
+a = Animal()
+# print(a) 
+print(Animal.pakshi)  
+print(Animal.janver) 
+    
+
