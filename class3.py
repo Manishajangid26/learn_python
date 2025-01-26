1e1a13cf3a05eff6cebccd4eec222c8b6c700fb4
Manishajangid26 <manishajangid12665@gmail.com>
2025-01-26 20:51:42 +0530

    Employee ID

---
+++ class3.py
@@ -0,0 +1,14 @@
+# class Employee:
+#     employee_id = 0
+
+# employee1 = Employee() 
+# employee2 = Employee()
+# employee1.employeeID = 1010
+# print("Employee ID=",(employee1.employeeID)) 
+
+
+class Employee:
+    a = input("enter employ id = ")
+
+employ=Employee()
+print("employ id =",Employee.a)    
