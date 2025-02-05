5dcbaa991935dd71d451e011a7b3158e2f6b2428
Manishajangid26 <manishajangid12665@gmail.com>
2025-02-05 20:13:21 +0530

    inbild functions in python

---
+++ inbild_function.py
@@ -0,0 +1,98 @@
+# 1.sum()
+
+# a = [1,2,5,4,6]
+# print(sum(a))
+
+
+# 2.zip()
+
+# a = [1,2,3,4,5,6]
+# b = ["manisha","vipin","aishwarya","rahul"]
+
+# print(list(zip(a,b)))
+
+
+# 3.sorted()
+
+# a = [2,3,7,8,4]
+
+# print(sorted(a))
+
+
+
+# 4.reversed()
+
+# a = [1,2,3,4,5,6]
+
+# print(list(reversed(a)))
+
+
+
+# 5.pow()
+
+# print(pow(2,10))
+
+
+
+# 6.oct()
+
+# a = [1,3,4,2,5]
+
+# print(oct(22))
+
+
+
+# 7.ord()
+
+# a = [1,2,3,47,5,6]
+
+# print(ord(a))
+
+
+
+# 8.max()
+
+# a = [1,2,3,4,33,22]
+
+# print(max(a))
+
+
+
+# 9.min()
+
+# a = [1,2,3,4,33,22]
+
+# print(min(a))
+
+
+# 10.callable()
+
+# a = [1,2,3,4,33,22]
+
+# def funa():
+
+#     print(callable(a))
+
+
+
+# 11.all()
+
+# a = [1,2,3,"",'',"manisha"]
+# a = [1,2,3,"manisha"]
+# print(all(a))
+
+
+
+
+# 12.any()
+
+# a = [1,2,3,"",'',"manisha"]
+
+# print(any(a))
+
+
+
+
+# 13.bin()
+
+# print(bin(2))
