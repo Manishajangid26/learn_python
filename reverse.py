8f6af0d5bef1417a5d9f62c3b5c676a923068b2f
manisha jangid <manisahjangid12665@gmail.com>
2025-02-11 17:02:41 +0530

    learing git CLI

--- reverse.py
+++ reverse.py
@@ -1,12 +1,23 @@
 # Write a function reverseNumber() that takes a positive integer as input and returns the reversed number. For example, if the input is 123, the function should return 321.
  
 
+# def rev_str(data):
+#     data = str(data)
+#     return data[ ::-1 ]
+
+
+# print(rev_str(12345))
+
+
+
+
 def rev_str(data):
     data = str(data)
     return data[ ::-1 ]
 
 
 print(rev_str(12345))
+
 
 
 
