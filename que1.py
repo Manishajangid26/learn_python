af454893af57c3416d960782d68579763fe16732
Manishajangid26 <manishajangid12665@gmail.com>
2025-02-15 18:19:26 +0530

    added a return list program

---
+++ que1.py
@@ -0,0 +1,10 @@
+#1. Write a Python function that accepts 3 different values as parameters and returns a list 
+# with all the values in it
+
+def number(a,b,c):
+   return [a,b,c]
+
+print(number(1,2,3))   
+
+   
+
