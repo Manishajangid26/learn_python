1e63141c7a2e6f5082d9dbef0d0a737d75e654ea
manisha jangid <manisahjangid12665@gmail.com>
2025-02-15 18:17:36 +0530

    added a return list

--- pratic_pro.py
+++ pratic_pro.py
@@ -1,8 +1,11 @@
 def no_boring_zeros(n):
-    return str(n).rstrip('0')
+    if n == 0:
+        return 0
+    return int(str(n).rstrip('0'))
     # number = int("enter number")
 
-result = no_boring_zeros(4506001)
+
+result = no_boring_zeros(0)
 print(result)
 
 
